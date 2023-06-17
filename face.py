4d4cd9630db2acb3a526bb80ec2309dfcf2f314b
Jayed Shibli <jayed990@gmail.com>
2023-06-17 15:16:17 +0600

    sql

--- face.py
+++ face.py
@@ -2,9 +2,11 @@ import cv2
 import numpy as np
 import face_recognition
 import os
-from datetime import datetime, date
+from datetime import datetime, date, timedelta
 from flask import Flask, flash, request, redirect, url_for, render_template, Response
 from werkzeug.utils import secure_filename
+import mysql.connector
+from mysql.connector import Error
 
 UPLOAD_FOLDER = 'data'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
@@ -46,6 +48,87 @@ def index():
     return render_template('index.html')
 
 
+# Function to encode images
+def encoding_img(data):
+    encodeList = []
+    for img in data:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encodeList.append(encode)
+    return encodeList
+
+
+# Function to establish a connection with the MySQL database
+def create_connection():
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host='JAYED',
+            database='attendance',
+            user='jayed',
+            password='1234'
+        )
+        print("Connected to MySQL database")
+    except Error as e:
+        print(f"The error '{e}' occurred while connecting to the MySQL database")
+    return connection
+
+
+# Function to create the attendance table if it doesn't exist
+def create_table(connection):
+    try:
+        cursor = connection.cursor()
+        create_table_query = """
+        CREATE TABLE IF NOT EXISTS attendance (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            name VARCHAR(255) NOT NULL,
+            time DATETIME NOT NULL
+        )
+        """
+        cursor.execute(create_table_query)
+        connection.commit()
+        print("Attendance table created successfully")
+    except Error as e:
+        print(f"The error '{e}' occurred while creating the attendance table")
+
+
+# Function to take attendance and insert into the MySQL database
+def takeAttendance(name, connection):
+    today = date.today()
+    # now = datetime.now().strftime('%H:%M:%S')
+    now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+    try:
+        cursor = connection.cursor()
+        insert_query = """
+        INSERT INTO attendance (name, time)
+        SELECT %s, %s
+        FROM DUAL
+        WHERE NOT EXISTS (
+            SELECT id
+            FROM attendance
+            WHERE name = %s
+            AND time > DATE_SUB(NOW(), INTERVAL 900 SECOND)
+        )
+        """
+        cursor.execute(insert_query, (name, now, name))
+        connection.commit()
+        print("Attendance recorded successfully")
+    except Error as e:
+        print(f"The error '{e}' occurred while inserting attendance record")
+
+
+# Main function to connect to the database and perform attendance operations
+def main():
+    connection = create_connection()
+    if connection is not None:
+        create_table(connection)
+        # Close the connection
+        if connection.is_connected():
+            connection.close()
+            print("MySQL database connection closed")
+
+
 def gen():
     data = []
     filename = []
@@ -58,38 +141,8 @@ def gen():
         data.append(img_path)
         filename.append(imagess.split(".", 1)[0])
 
-    # Function to encode images
-    def encoding_img(data):
-        encodeList = []
-        for img in data:
-            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-            encode = face_recognition.face_encodings(img)[0]
-            encodeList.append(encode)
-        return encodeList
-
-    # Function to take attendance and write to CSV file
-    def takeAttendance(name):
-        today = date.today()
-        filename = today.strftime("%d-%m-%Y") + '.csv'
-        with open(filename, 'a+') as f:
-            f.seek(0)
-            mypeople_list = f.readlines()
-            nameList = []
-            for line in mypeople_list:
-                entry = line.split(',')
-                nameList.append(entry[0])
-            if name not in nameList:
-                now = datetime.now()
-                datestring = now.strftime('%H:%M:%S')
-                f.writelines(f'\n{name},{datestring}')
-
-                # Add column headers if the file is empty
-                if f.tell() == 0:
-                    f.writelines("name,time")
-                    f.writelines(f'\n{name},{datestring}')
-
     encodeListknown = encoding_img(data)
-    cap = cv2.VideoCapture(1)
+    cap = cv2.VideoCapture(0)
 
     while True:
         success, img = cap.read()
@@ -112,7 +165,12 @@ def gen():
                     cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 0), 2)
                     cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (255, 0, 0), 2, cv2.FILLED)
                     cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
-                    takeAttendance(name)
+                    connection = create_connection()
+                    if connection is not None:
+                        takeAttendance(name, connection)
+                        if connection.is_connected():
+                            connection.close()
+                            print("MySQL database connection closed")
 
         # Convert image to JPEG format for streaming
         frame = cv2.imencode('.jpg', img)[1].tobytes()
@@ -130,4 +188,5 @@ def video_feed():
 
 
 if __name__ == "__main__":
+    main()
     app.run(debug=True)
