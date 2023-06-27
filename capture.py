0ef0ac45e532415aae0342037b058483bc481d56
Jayed Shibli <jayed990@gmail.com>
2023-06-27 07:31:24 +0600

    haarcascade_frontalface

---
+++ capture.py
@@ -0,0 +1,56 @@
+import cv2
+import os
+import time
+
+# Function to capture images
+def capture_images(name, output_folder):
+    # Create a directory for the person if it doesn't exist
+    person_folder = os.path.join(output_folder, name)
+    os.makedirs(person_folder, exist_ok=True)
+
+    # Initialize the webcam
+    cap = cv2.VideoCapture(0)
+
+    # Capture 20 images
+    count = 0
+    while count < 20:
+        ret, frame = cap.read()
+
+        # Display the frame
+        cv2.imshow('Capture Images', frame)
+
+        # Introduce a delay of 0.33 seconds
+        time.sleep(0.33)
+
+        # Save the frame as an image
+        image_path = os.path.join(person_folder, f'{name}_{count}.jpg')
+        cv2.imwrite(image_path, frame)
+
+        count += 1
+
+        # Break the loop when 20 images are captured
+        if count == 20:
+            break
+
+        # Break the loop and move to the next person when 'q' is pressed
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # Release the webcam and destroy the window
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+# Define the output folder
+output_folder = 'data'
+
+# Get user input for person names
+while True:
+    name = input("Enter the name of the person (or 'q' to quit): ")
+    if name.lower() == 'q':
+        break
+
+    # Capture images for the person
+    capture_images(name, output_folder)
+
+print("Image capture completed.")
--- face.py
+++ face.py
@@ -1,20 +1,14 @@
 import cv2
 import numpy as np
-import face_recognition
 import os
 from datetime import datetime, date, timedelta
-from flask import Flask, flash, request, redirect, url_for, render_template, Response
-from werkzeug.utils import secure_filename
+from flask import Flask, render_template, Response
 import mysql.connector
 from mysql.connector import Error
 
+
 UPLOAD_FOLDER = 'data'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
-
-# Function to check if the file has an allowed extension
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 app = Flask(__name__)
@@ -22,40 +16,16 @@ app.secret_key = "secret key"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
+# Function to check if the file has an allowed extension
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
 @app.route('/')
-def upload_file():
-    return render_template('upload.html')
-
-
-@app.route('/success', methods=['GET', 'POST'])
-def success():
-    if 'file' not in request.files:
-        return render_template('upload.html')
-    file = request.files['file']
-    if file.filename == '':
-        return render_template('upload.html')
-    if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        return render_template('upload.html')
-    else:
-        return render_template('upload.html')
-
-
-@app.route('/index')
 def index():
     """Video streaming home page."""
     return render_template('index.html')
-
-
-# Function to encode images
-def encoding_img(data):
-    encodeList = []
-    for img in data:
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        encode = face_recognition.face_encodings(img)[0]
-        encodeList.append(encode)
-    return encodeList
 
 
 # Function to establish a connection with the MySQL database
@@ -111,7 +81,6 @@ def create_tables(connection):
         print("Attendance, Time, and Anomaly tables created successfully")
     except Error as e:
         print(f"The error '{e}' occurred while creating the tables")
-
 
 
 # Function to calculate the total time in the office
@@ -171,7 +140,7 @@ def takeAttendance(name, connection):
             """
             cursor.execute(update_query, (now, time_id))
             connection.commit()
-            print("Outtime updated successfully")
+            # print("Outtime updated successfully")
         else:
             # If the employee doesn't have an entry for the current date, insert a new row with intime
             insert_query = """
@@ -180,8 +149,8 @@ def takeAttendance(name, connection):
             """
             cursor.execute(insert_query, (name, now))
             connection.commit()
-            print("Intime recorded successfully")
-            
+            # print("Intime recorded successfully")
+
             # Retrieve the newly inserted time_id
             time_id = cursor.lastrowid
 
@@ -201,7 +170,7 @@ def takeAttendance(name, connection):
         """
         cursor.execute(update_total_time_query, (total_time, time_id))
         connection.commit()
-        print("Total time updated successfully")
+        # print("Total time updated successfully")
 
         # Insert the attendance record
         insert_attendance_query = """
@@ -217,7 +186,7 @@ def takeAttendance(name, connection):
         """
         cursor.execute(insert_attendance_query, (name, now, name))
         connection.commit()
-        print("Attendance recorded successfully")
+        # print("Attendance recorded successfully")
 
         # Check if the total time is an anomaly and occurred more than 24 hours ago
         if total_time and is_anomaly(total_time) and (now - updated_intime) > timedelta(hours=24):
@@ -234,67 +203,76 @@ def takeAttendance(name, connection):
         print(f"The error '{e}' occurred while inserting attendance record")
 
 
-
-# Main function to connect to the database and perform attendance operations
-def main():
-    connection = create_connection()
-    if connection is not None:
-        create_tables(connection)
-        # Close the connection
-        if connection.is_connected():
-            connection.close()
-            print("MySQL database connection closed")
-
-
 def gen():
     data = []
-    filename = []
     dir_path = 'data'
 
-    # Load images and filenames from the directory
-    for imagess in os.listdir(dir_path):
-        img_path = os.path.join(dir_path, imagess)
-        img_path = face_recognition.load_image_file(img_path)
-        data.append(img_path)
-        filename.append(imagess.split(".", 1)[0])
+    # Load images and folder names from the directory
+    for person_folder in os.listdir(dir_path):
+        person_folder_path = os.path.join(dir_path, person_folder)
+        if os.path.isdir(person_folder_path):
+            for imagess in os.listdir(person_folder_path):
+                img_path = os.path.join(person_folder_path, imagess)
+                img = cv2.imread(img_path)
+                data.append(img)
 
-    encodeListknown = encoding_img(data)
     cap = cv2.VideoCapture(0)
+
+    consecutive_frames_required = 5  # Number of consecutive frames required for a valid match
+    consecutive_frames_detected = 0  # Counter for consecutive frames with a detected face
+    match_found = False  # Flag to indicate if a match has been found
 
     while True:
         success, img = cap.read()
-        imgc = cv2.resize(img, (0, 0), None, 0.25, 0.25)
-        imgc = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-        fasescurrent = face_recognition.face_locations(imgc)
-        encode_fasescurrent = face_recognition.face_encodings(imgc, fasescurrent)
+        # Detect faces in the grayscale image
+        face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+        faces = face_cascade.detectMultiScale(img_gray, 1.3, 5)
 
-        # Compare faces in the current frame with known faces
-        for encodeFace, faceloc in zip(encode_fasescurrent, fasescurrent):
-            face_distence = face_recognition.face_distance(encodeListknown, encodeFace)
-            if len(face_distence) > 0:
-                matchindex = np.argmin(face_distence)
-                matches_face = face_recognition.compare_faces(encodeListknown, encodeFace)
+        if len(faces) > 0:
+            consecutive_frames_detected += 1
+        else:
+            consecutive_frames_detected = 0
 
-                if matches_face[matchindex]:
-                    name = filename[matchindex].upper()
-                    y1, x2, y2, x1 = faceloc
-                    cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 0), 2)
-                    cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (255, 0, 0), 2, cv2.FILLED)
-                    cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
-                    connection = create_connection()
-                    if connection is not None:
-                        takeAttendance(name, connection)
-                        if connection.is_connected():
-                            connection.close()
-                            print("MySQL database connection closed")
+        if consecutive_frames_detected == consecutive_frames_required:
+            # Face detected in multiple consecutive frames
+            for (x, y, w, h) in faces:
+                roi_gray = img_gray[y:y + h, x:x + w]
 
-        # Convert image to JPEG format for streaming
-        frame = cv2.imencode('.jpg', img)[1].tobytes()
-        yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-        key = cv2.waitKey(20)
-        if key == 27:
-            break
+                # Flag to check if a match is found
+                match_found = False
+
+                for stored_img in data:
+                    stored_img_gray = cv2.cvtColor(stored_img, cv2.COLOR_BGR2GRAY)
+                    stored_img_gray = cv2.resize(stored_img_gray, (w, h))
+                    diff = cv2.absdiff(roi_gray, stored_img_gray)
+                    diff_mean = np.mean(diff)
+                    matching_percentage = (1 - diff_mean / 255) * 100
+
+                    if matching_percentage > 80:
+                        takeAttendance(person_folder, connection)
+                        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 3)
+                        cv2.putText(img, f"{person_folder} ({matching_percentage:.2f}%)", (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+                        match_found = True
+                        break
+
+                if not match_found:
+                    # If no match is found, display "Unknown"
+                    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 3)
+                    cv2.putText(img, "Unknown", (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 255), 2)
+
+        elif consecutive_frames_detected < consecutive_frames_required:
+            # Reset the match_found flag and consecutive_frames_detected counter
+            match_found = False
+
+        ret, buffer = cv2.imencode('.jpg', img)
+        frame = buffer.tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+    cap.release()
+
 
 
 @app.route('/video_feed')
@@ -304,6 +282,7 @@ def video_feed():
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    connection = create_connection()
+    create_tables(connection)
     app.run(debug=True)
